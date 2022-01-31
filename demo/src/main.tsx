c4e7737482b51934c6c4797da70417d589c5c44e
Vilius <vilius.kazlauskas@bentley.com>
2022-01-31 14:32:26 +0200

    resets

--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 
 ReactDOM.render(
