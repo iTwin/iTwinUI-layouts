4878199ac702709d4ccc200cf1f38bdf96bb17c2
Jon Graft <FlyersPh9@gmail.com>
2022-02-09 15:04:20 -0500

    Using display block on iframe.

--- demo/src/demos/PageLayoutDemo.tsx
+++ demo/src/demos/PageLayoutDemo.tsx
@@ -24,7 +24,12 @@ export const PageLayoutDemo = () => {
           <iframe
             src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d391622.38536755356!2d-75.43763126705527!3d39.92908811024802!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c6f534e0393163%3A0x1eb84dcc94686b39!2sBentley%20Systems%20Inc!5e0!3m2!1sen!2sus!4v1644333059464!5m2!1sen!2sus'
             loading='lazy'
-            style={{ border: 'none', width: '100%', height: '100%' }}
+            style={{
+              border: 'none',
+              width: '100%',
+              height: '100%',
+              display: 'block',
+            }}
           />
         </PageLayout.Content>
       </PageLayout>
