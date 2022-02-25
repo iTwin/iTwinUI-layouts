5907cf0284757301ba0f2c13a7b51faac4192ed9
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2022-02-25 10:29:16 -0500

    First template (#11)
    - Populated the header component to better represent a realistic scenario.
    - Added embedded iTwin.js demo.
    - Updated icons to 1.7.0.

--- demo/src/common/DemoHeader.tsx
+++ demo/src/common/DemoHeader.tsx
@@ -8,64 +8,193 @@ import {
   HeaderLogo,
   HeaderBreadcrumbs,
   HeaderButton,
+  MenuDivider,
+  MenuExtraContent,
   MenuItem,
   UserIcon,
   IconButton,
+  DropdownMenu,
+  Select,
+  Text,
 } from '@itwin/itwinui-react';
-import { ReactComponent as Logo } from './itwin.svg';
-import { SvgNetwork } from '@itwin/itwinui-icons-react';
+import {
+  SvgCheckmark,
+  SvgModel,
+  SvgNotification,
+  SvgHelpCircular,
+  SvgInfoCircular,
+  SvgSmileyHappy,
+  SvgSettings,
+  SvgImodelHollow,
+  SvgExit,
+  SvgNews,
+  SvgProject,
+} from '@itwin/itwinui-icons-react';
 
 export const DemoHeader = () => {
   return (
     <Header
-      appLogo={<HeaderLogo logo={<Logo />}>iTwin Services</HeaderLogo>}
+      appLogo={
+        <HeaderLogo logo={<SvgImodelHollow />}>iTwin Services</HeaderLogo>
+      }
       breadcrumbs={
         <HeaderBreadcrumbs
           items={[
             <HeaderButton
               key='projectBreadcrumb'
-              menuItems={() => [
-                <MenuItem key='projectA'>Project A</MenuItem>,
-                <MenuItem key='projectB'>Project B</MenuItem>,
-              ]}
-              name='Project A'
-              description='789879'
-              startIcon={<SvgNetwork />}
-            />,
-            <HeaderButton
-              key='iModelBreadcrumb'
-              menuItems={() => [
-                <MenuItem key='iModelA'>iModel A</MenuItem>,
-                <MenuItem key='iModelB'>iModel B</MenuItem>,
-              ]}
-              name='iModel A'
-              description='5456465'
+              name='Project Alpha'
+              description='0x0123456789'
               startIcon={
                 <img
                   src='https://itwinplatformcdn.azureedge.net/iTwinUI/stadium.png'
                   alt='Project thumbnail'
+                  draggable='false'
                 />
               }
+              onClick={() => {}}
+              menuItems={() => [
+                <MenuItem
+                  key='projectA'
+                  sublabel='0x0123456789'
+                  icon={
+                    <img
+                      src='https://itwinplatformcdn.azureedge.net/iTwinUI/stadium.png'
+                      alt='Project thumbnail'
+                      draggable='false'
+                    />
+                  }
+                  badge={<SvgCheckmark />}
+                  isSelected
+                >
+                  Project Alpha
+                </MenuItem>,
+                <MenuItem
+                  key='projectB'
+                  sublabel='0x0987654321'
+                  icon={<SvgProject />}
+                >
+                  Project Beta
+                </MenuItem>,
+                <MenuItem
+                  key='projectC'
+                  sublabel='0x0001337420'
+                  icon={<SvgProject />}
+                >
+                  Project Charlie
+                </MenuItem>,
+                <MenuDivider key='divider' />,
+                <MenuItem key='myProjects'>My projects</MenuItem>,
+              ]}
+            />,
+            <HeaderButton
+              key='iModelBreadcrumb'
+              name='iModel Beta'
+              description='0x0987654321'
+              startIcon={<SvgModel />}
+              onClick={() => {}}
+              isActive
+              menuItems={() => [
+                <MenuItem
+                  key='iModelA'
+                  sublabel='0x0123456789'
+                  icon={<SvgModel />}
+                >
+                  iModel Alpha
+                </MenuItem>,
+                <MenuItem
+                  key='iModelB'
+                  sublabel='0x0987654321'
+                  icon={<SvgModel />}
+                  isSelected
+                  badge={<SvgCheckmark />}
+                >
+                  iModel Beta
+                </MenuItem>,
+                <MenuItem
+                  key='iModelC'
+                  sublabel='0x0001337420'
+                  icon={<SvgModel />}
+                >
+                  iModel Charlie
+                </MenuItem>,
+                <MenuDivider key='divider' />,
+                <MenuItem key='myiModels'>My iModels</MenuItem>,
+              ]}
             />,
           ]}
         />
       }
+      actions={[
+        <IconButton key='notifications' styleType='borderless'>
+          <SvgNotification />
+        </IconButton>,
+        <DropdownMenu
+          key='help'
+          menuItems={() => [
+            <MenuItem key='getting-started'>Getting started</MenuItem>,
+            <MenuItem key='report-a-problem'>Report a problem</MenuItem>,
+            <MenuItem key='communities'>Communities</MenuItem>,
+          ]}
+        >
+          <IconButton styleType='borderless'>
+            <SvgHelpCircular />
+          </IconButton>
+        </DropdownMenu>,
+      ]}
       userIcon={
-        <IconButton styleType='borderless'>
-          <UserIcon
-            size='medium'
-            image={
-              <img
-                src='https://itwinplatformcdn.azureedge.net/iTwinUI/user-placeholder.png'
-                alt='User icon'
-              />
-            }
-          />
-        </IconButton>
+        <DropdownMenu
+          key='profile'
+          role='menu'
+          menuItems={() => [
+            <MenuExtraContent key={0}>
+              <>
+                <Text variant='leading'>Terry Rivers</Text>
+                <Text isMuted style={{ marginBottom: 8 }}>
+                  terry.rivers@email.com
+                </Text>
+                <Select
+                  options={[
+                    { value: 'User', label: 'User' },
+                    { value: 'Moderator', label: 'Moderator' },
+                    { value: 'Administrator', label: 'Administrator' },
+                  ]}
+                  value={'Administrator'}
+                />
+              </>
+            </MenuExtraContent>,
+            <MenuDivider key={1} />,
+            <MenuItem key='view-profile'>View profile</MenuItem>,
+            <MenuItem key='sign-out' badge={<SvgExit />}>
+              Sign out
+            </MenuItem>,
+          ]}
+        >
+          <IconButton styleType='borderless'>
+            <UserIcon
+              size='medium'
+              image={
+                <img
+                  src='https://itwinplatformcdn.azureedge.net/iTwinUI/user-placeholder.png'
+                  alt='Terry Rivers'
+                />
+              }
+            />
+          </IconButton>
+        </DropdownMenu>
       }
       menuItems={() => [
-        <MenuItem key='moreoption1'>Menu item #1</MenuItem>,
-        <MenuItem key='moreoption2'>Menu item #2</MenuItem>,
+        <MenuItem key='settings' icon={<SvgSettings />}>
+          Settings
+        </MenuItem>,
+        <MenuItem key='feedback' icon={<SvgSmileyHappy />}>
+          Feedback
+        </MenuItem>,
+        <MenuItem key='whats-new' icon={<SvgNews />}>
+          {`What's new`}
+        </MenuItem>,
+        <MenuItem key='about' icon={<SvgInfoCircular />}>
+          About
+        </MenuItem>,
       ]}
     />
   );
