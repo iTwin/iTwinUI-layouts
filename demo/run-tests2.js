8d735e0c06172fe93183943a8c983df0704b015e
Vilius <vilius.kazlauskas@bentley.com>
2022-02-25 15:43:04 +0200

    trying to run server from node

---
+++ demo/run-tests2.js
@@ -0,0 +1,30 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+const { spawn } = require('child_process');
+
+const serverProcess = spawn('yarn dev', {
+  stdio: 'inherit',
+  shell: true,
+});
+
+const dockerProcess = spawn(
+  `docker run --rm -v "${__dirname}/../":/e2e -w /e2e/demo cypress/included:9.5.0 --browser chrome --headless `,
+  {
+    stdio: 'inherit',
+    shell: true,
+  },
+);
+dockerProcess.on('error', (error) => {
+  process.exitCode = 1;
+  console.error(`Tests run failed with error: ${error}`);
+  serverProcess.kill();
+});
+dockerProcess.on('exit', (code) => {
+  if (code !== 0) {
+    process.exitCode = code;
+    console.error(`Tests run failed with code: ${code}`);
+    serverProcess.kill();
+  }
+});
