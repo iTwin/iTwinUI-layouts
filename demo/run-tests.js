da80263bd2840270c4facdeae512e3a9e00b4187
Vilius <vilius.kazlauskas@bentley.com>
2022-02-28 12:36:17 +0200

    Simplified things a bit

--- demo/run-tests.js
+++ demo/run-tests.js
@@ -4,16 +4,13 @@
  *--------------------------------------------------------------------------------------------*/
 const { spawn } = require('child_process');
 
-const args = process.argv.slice(2);
-const isCi = args.includes('--ci');
-
-const DOCKER_COMMAND = `docker run --rm --add-host host.docker.internal:host-gateway -v "${__dirname}/../":/e2e -w /e2e/demo cypress/included:9.5.0 --browser chrome --headless `;
-const DOCKER_COMMAND_CI = `docker run --rm --entrypoint /bin/bash -e CYPRESS_baseUrl=http://localhost:5000/iTwinUI-layouts/ -v "${__dirname}/../":/e2e -w /e2e cypress/included:9.5.0 ./demo/entrypoint.sh`;
-
-const dockerProcess = spawn(isCi ? DOCKER_COMMAND_CI : DOCKER_COMMAND, {
-  stdio: 'inherit',
-  shell: true,
-});
+const dockerProcess = spawn(
+  `docker run --rm --entrypoint ./demo/entrypoint.sh -v "${__dirname}/../":/e2e -w /e2e cypress/included:9.5.0`,
+  {
+    stdio: 'inherit',
+    shell: true,
+  },
+);
 dockerProcess.on('error', (error) => {
   process.exitCode = 1;
   console.error(`Tests run failed with error: ${error}`);
