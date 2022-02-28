f90012d1e74549b0d70d48211bc557ddd3083b55
Vilius <vilius.kazlauskas@bentley.com>
2022-02-28 12:51:06 +0200

    Explaining docker commands

--- demo/run-tests.js
+++ demo/run-tests.js
@@ -4,7 +4,14 @@
  *--------------------------------------------------------------------------------------------*/
 const { spawn } = require('child_process');
 
+// Need to use this script because current directory variable is different in different shells
 const dockerProcess = spawn(
+  // --rm - removes container after run
+  // --entrypoint /bin/bash - overrides the default entrypoint (command that is ran) of the cypress image
+  // -v "${__dirname}/../":/e2e - mount a root repo directory from host (your PC) to container
+  // -w /e2e - makes `e2e` the working directory
+  // cypress/included:9.5.0 - Docker image to run
+  // ./demo/entrypoint.sh - entrypoint script to run
   `docker run --rm --entrypoint /bin/bash -v "${__dirname}/../":/e2e -w /e2e cypress/included:9.5.0 ./demo/entrypoint.sh`,
   {
     stdio: 'inherit',
