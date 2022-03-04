ae821a0f75fcc53761be06dadbb48c1be26555cc
Vilius <vilius.kazlauskas@bentley.com>
2022-03-04 12:16:04 +0200

    Using HashRouter

--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -4,21 +4,21 @@
  *--------------------------------------------------------------------------------------------*/
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './index.scss';
 import App from './App';
 import { DEMOS_LIST } from './demos/list';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename='/iTwinUI-layouts/'>
+    <HashRouter>
       <Routes>
-        <Route path='' element={<App />} />
+        <Route path='/' element={<App />} />
         {DEMOS_LIST.map(({ path, component }) => (
-          <Route key={path} path={`/${path}`} element={component()} />
+          <Route key={path} path={path} element={component()} />
         ))}
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
