2f2acf10a4e3216136d5a8b25caea42048f6bcb4
Greta <greta.nausedaite@bentley.com>
2022-03-07 12:21:45 +0200

    adding tiles with thumbnails

--- demo/src/demos/GridLayoutDemo.tsx
+++ demo/src/demos/GridLayoutDemo.tsx
@@ -7,7 +7,8 @@ import { DemoHeader } from '../common/DemoHeader';
 import { DemoSideNav } from '../common/DemoSideNav';
 import { DemoTemplate } from '../common/DemoTemplate';
 import { PageLayout } from '@itwin/itwinui-layouts-react';
-import { Tile, Text, Subheading } from '@itwin/itwinui-react';
+import { Tile, Subheading } from '@itwin/itwinui-react';
+import SvgImodelHollow from '@itwin/itwinui-icons-react/cjs/icons/ImodelHollow';
 
 export const GridLayoutDemo = () => {
   return (
@@ -26,14 +27,46 @@ export const GridLayoutDemo = () => {
             <div>
               <Subheading>Grid</Subheading>
               <PageLayout.Grid>
-                <Tile name='Tile 1' />
-                <Tile name='Tile 1' />
-                <Tile name='Tile 1' />
-                <Tile name='Tile 1' />
-                <Tile name='Tile 1' />
-                <Tile name='Tile 1' />
-                <Tile name='Tile 1' />
-                <Tile name='Tile 1' />
+                <Tile
+                  name='Tile name'
+                  description='Description #1'
+                  thumbnail={<SvgImodelHollow />}
+                />
+                <Tile
+                  name='Tile name'
+                  description='Description #2'
+                  thumbnail={<SvgImodelHollow />}
+                />
+                <Tile
+                  name='Tile name'
+                  description='Description #3'
+                  thumbnail={<SvgImodelHollow />}
+                />
+                <Tile
+                  name='Tile name'
+                  description='Description #4'
+                  thumbnail={<SvgImodelHollow />}
+                />
+                <Tile
+                  name='Tile name'
+                  description='Description #5'
+                  thumbnail={<SvgImodelHollow />}
+                />
+                <Tile
+                  name='Tile name'
+                  description='Description #6'
+                  thumbnail={<SvgImodelHollow />}
+                />
+                <Tile
+                  name='Tile name'
+                  description='Description #7'
+                  thumbnail={<SvgImodelHollow />}
+                />
+                <Tile
+                  name='Tile name'
+                  description='Description #8'
+                  thumbnail={<SvgImodelHollow />}
+                />
               </PageLayout.Grid>
             </div>
           </PageLayout.PaddedContent>
