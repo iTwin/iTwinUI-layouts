1f12b9af1e92eaaac1bcb8aa92c50ac2c08e7c7c
Greta <greta.nausedaite@bentley.com>
2022-03-24 14:02:48 +0200

    comment fix

--- demo/run-tests-m1.js
+++ demo/run-tests-m1.js
@@ -11,7 +11,7 @@ const dockerProcess = spawn(
   // -e WIDTH=3840 -e HEIGHT=2160 - sets the width and height of the browser window, required for headless Chrome
   // -v "${__dirname}/../":/e2e - mount a root repo directory from host (your PC) to container
   // -w /e2e - makes `e2e` the working directory
-  // cypress/included:9.5.0 - Docker image to run
+  // cookiescrumbs/cypress-m1:9.4.1 - Docker image to run
   // ./demo/entrypoint.sh - entrypoint script to run
   `docker run --rm --entrypoint /bin/bash -e WIDTH=3840 -e HEIGHT=2160 -v "${__dirname}/../":/e2e -w /e2e cookiescrumbs/cypress-m1:9.4.1 ./demo/entrypoint.sh`,
   {
