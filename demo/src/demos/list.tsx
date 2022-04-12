52b00a06b2cd4a528191765f9909fc996241be79
Jon Graft <FlyersPh9@gmail.com>
2022-04-12 13:33:57 -0400

    Initial push.

--- demo/src/demos/list.tsx
+++ demo/src/demos/list.tsx
@@ -13,14 +13,14 @@ export const DEMOS_LIST = [
     path: 'app-layout',
     name: 'App',
     description: 'Display an iTwin.js frame within the content area.',
-    icon: '/layouts-thumbnails/AppLayout.png',
+    icon: 'layouts-thumbnails/AppLayout.png',
     component: AppLayoutDemo,
   },
   {
     path: 'grid-layout',
     name: 'Tiles grid',
     description: 'A responsive grid of tiles.',
-    icon: '/layouts-thumbnails/TileGridLayout.png',
+    icon: 'layouts-thumbnails/TileGridLayout.png',
     component: GridLayoutDemo,
   },
 ];
