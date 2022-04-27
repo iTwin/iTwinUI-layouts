35db00efa313d01bcb5cee38ea40276d13dbaafb
Greta <greta.nausedaite@bentley.com>
2022-04-27 15:37:52 +0300

    added comments about screen sizes in grid types

--- packages/layouts-react/src/components/Grid/types.tsx
+++ packages/layouts-react/src/components/Grid/types.tsx
@@ -4,17 +4,17 @@
  *--------------------------------------------------------------------------------------------*/
 
 export type ScreenSizes = {
-  mobile: number;
-  landscapeMobile: number;
-  tablet: number;
-  smallMonitor: number;
-  monitor: number;
+  mobile: number; // screen < 486px
+  landscapeMobile: number; // 487px < screen < 768px
+  tablet: number; // 769px < screen < 991px
+  smallMonitor: number; // 992px < screen < 1399px
+  monitor: number; // 1400px < screen
 };
 
 export type ScreenSizesOffset = {
-  mobile: 'auto' | number;
-  landscapeMobile: 'auto' | number;
-  tablet: 'auto' | number;
-  smallMonitor: 'auto' | number;
-  monitor: 'auto' | number;
+  mobile: 'auto' | number; // screen < 486px
+  landscapeMobile: 'auto' | number; // 487px < screen < 768px
+  tablet: 'auto' | number; // 769px < screen < 991px
+  smallMonitor: 'auto' | number; // 992px < screen < 1399px
+  monitor: 'auto' | number; // 1400px < screen
 };
