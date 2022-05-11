905571141eea007b29911e1669d9f182ac04c7a7
Greta <greta.nausedaite@bentley.com>
2022-05-11 14:14:24 +0300

    Remove types dep

--- demo/src/types.d.ts
+++ demo/src/types.d.ts
@@ -27,3 +27,88 @@ declare global {
     }
   }
 }
+
+// Type definitions for react-syntax-highlighter 13.5
+// Project: https://github.com/conorhastings/react-syntax-highlighter
+// Definitions by: Ivo Stratev <https://github.com/NoHomey>
+//                 Guo Yunhe <https://github.com/guoyunhe>
+//                 Anirban Sengupta <https://github.com/anirban09>
+//                 Michael Yuen <https://github.com/michaelyuen>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8
+
+type lineTagPropsFunction = (
+  lineNumber: number,
+) => React.HTMLProps<HTMLElement>;
+
+declare module 'react-syntax-highlighter' {
+  export interface SyntaxHighlighterProps {
+    language?: string | undefined;
+    style?: any;
+    customStyle?: any;
+    lineProps?: lineTagPropsFunction | React.HTMLProps<HTMLElement> | undefined;
+    codeTagProps?: React.HTMLProps<HTMLElement> | undefined;
+    useInlineStyles?: boolean | undefined;
+    showLineNumbers?: boolean | undefined;
+    startingLineNumber?: number | undefined;
+    lineNumberStyle?: any;
+    [spread: string]: any;
+  }
+
+  export { default } from 'react-syntax-highlighter/dist/esm/default-highlight';
+  export { default as Prism } from 'react-syntax-highlighter/dist/esm/prism';
+}
+
+declare module 'react-syntax-highlighter/dist/esm/default-highlight' {
+  import * as React from 'react';
+  import { SyntaxHighlighterProps } from 'react-syntax-highlighter';
+  export default class SyntaxHighlighter extends React.Component<SyntaxHighlighterProps> {
+    static supportedLanguages: string[];
+  }
+}
+
+declare module 'react-syntax-highlighter/dist/esm/prism' {
+  import * as React from 'react';
+  import { SyntaxHighlighterProps } from 'react-syntax-highlighter';
+  export default class SyntaxHighlighter extends React.Component<SyntaxHighlighterProps> {
+    static supportedLanguages: string[];
+  }
+}
+
+declare module 'react-syntax-highlighter/dist/esm/styles/prism' {
+  export { default as coy } from 'react-syntax-highlighter/dist/esm/styles/prism/coy';
+  export { default as darcula } from 'react-syntax-highlighter/dist/esm/styles/prism/darcula';
+  export { default as vs } from 'react-syntax-highlighter/dist/esm/styles/prism/vs';
+}
+
+declare module 'react-syntax-highlighter/dist/esm/styles/prism/coy' {
+  const style: any;
+  export default style;
+}
+
+declare module 'react-syntax-highlighter/dist/esm/styles/prism/darcula' {
+  const style: any;
+  export default style;
+}
+
+declare module 'react-syntax-highlighter/dist/esm/styles/prism/vs' {
+  const style: any;
+  export default style;
+}
+
+declare module 'react-syntax-highlighter/dist/esm/languages/hljs/typescript' {
+  const language: any;
+  export default language;
+}
+declare module 'react-syntax-highlighter/dist/esm/languages/hljs/javascript' {
+  const language: any;
+  export default language;
+}
+declare module 'react-syntax-highlighter/dist/esm/languages/hljs/html' {
+  const language: any;
+  export default language;
+}
+declare module 'react-syntax-highlighter/dist/esm/languages/hljs/json' {
+  const language: any;
+  export default language;
+}
