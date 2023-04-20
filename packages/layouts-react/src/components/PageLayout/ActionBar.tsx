e607ccef753bbada29c4eed75ab57693fee6c382
Annie <22462084+LostABike@users.noreply.github.com>
2023-04-20 12:55:09 -0500

    revert previous changes and updated ActionBar and height

--- packages/layouts-react/src/components/PageLayout/ActionBar.tsx
+++ packages/layouts-react/src/components/PageLayout/ActionBar.tsx
@@ -11,15 +11,30 @@ export type ActionBarProps = {
    * Children of the element
    */
   children: React.ReactNode;
+  /**
+   * Left side of button bar
+   */
+  leftPart?: React.ReactNode;
+  /**
+   * Middle part of button bar
+   */
+  middlePart?: React.ReactNode;
+  /**
+   * Right side of button bar
+   */
+  rightPart?: React.ReactNode;
 } & StylingProps;
 
 export const ActionBar = (props: ActionBarProps) => {
-  const { className, style, children } = props;
+  const { className, style, children, leftPart, middlePart, rightPart } = props;
   return (
     <div
       className={cx('iui-layouts-page-content-action-bar', className)}
       style={style}
     >
+      {leftPart && <div className='action-bar-left'>{leftPart}</div>}
+      {middlePart && <div className='action-bar-middle'>{middlePart}</div>}
+      {rightPart && <div className='action-bar-right'>{rightPart}</div>}
       {children}
     </div>
   );
